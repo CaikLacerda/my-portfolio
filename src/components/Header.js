8879066632dc52f3e51082c0a036774d2e3500d2
Caik D. Lacerda <caiklacerda9@gmail.com>
2024-08-22 23:25:28 -0300

    Inicio da criacao do cabecalho

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Header.css'
 
 function Header() {
     return(
-        <div>
-            
+        <div className='header'>
+            <span className='name-header'>&lt;Caik Lacerda /&gt;</span>
+            <nav className='nav-header'>
+                <ul className='list-header'>
+                    <li className='above-me'><a href="#">Sobre mim</a></li>
+                    <li className='projects'><a href="#">Projetos</a></li>
+                    <li className='contacts'><a href="#">Contatos</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
